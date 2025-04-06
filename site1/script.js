11fdb7e6cfe7806953fba68dcb2875d5f369eecd
BssCreate <bed_random@mail.ru>
2025-04-06 22:39:22 +0700

    Update script.js

--- site1/script.js
+++ site1/script.js
@@ -1,9 +1,14 @@
 fetch('/api/site1')
-  .then(res => res.json())
+  .then(res => {
+    if (!res.ok) {
+      return res.json().then(error => { throw new Error(error.error || 'Ошибка сервера'); });
+    }
+    return res.json();
+  })
   .then(data => {
     document.getElementById('data').textContent = JSON.stringify(data, null, 2);
   })
   .catch(err => {
-    document.getElementById('data').textContent = 'Ошибка загрузки данных';
+    document.getElementById('data').textContent = 'Ошибка загрузки данных: ' + err.message;
     console.error(err);
   });
